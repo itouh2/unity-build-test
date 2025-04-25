a961c15e17ec0390070707100da39c642dfea2e1
itouh2 <itouh2@iola.jp>
2025-04-25 22:05:41 +0900

    feat: Androidビルドの追加

--- UnityProject/Assets/Scripts/Editor/BuildScript.cs
+++ UnityProject/Assets/Scripts/Editor/BuildScript.cs
@@ -2,21 +2,19 @@ using UnityEditor;
 using UnityEditor.Build.Reporting;
 using System;
 using System.IO;
-using System.Linq; // 重要：GetScenePathsメソッドで必要
+using System.Linq;
 
 public class BuildScript
 {
     // iOSビルド用メソッド
     public static void BuildIOS()
     {
-        // ビルド設定
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
         buildPlayerOptions.scenes = GetScenePaths();
         buildPlayerOptions.locationPathName = "Builds/iOS";
         buildPlayerOptions.target = BuildTarget.iOS;
         buildPlayerOptions.options = BuildOptions.None;
 
-        // ビルド実行
         BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
         BuildSummary summary = report.summary;
 
@@ -27,6 +25,29 @@ public class BuildScript
         else
         {
             Console.WriteLine("iOS build failed");
+            EditorApplication.Exit(1);
+        }
+    }
+
+    // Androidビルド用メソッド
+    public static void BuildAndroid()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = GetScenePaths();
+        buildPlayerOptions.locationPathName = "Builds/Android/app.apk"; // 出力APKのパス
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Console.WriteLine("Android build succeeded: " + summary.totalSize + " bytes");
+        }
+        else
+        {
+            Console.WriteLine("Android build failed");
             EditorApplication.Exit(1);
         }
     }
